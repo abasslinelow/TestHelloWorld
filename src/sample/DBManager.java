cd2d785f4ea9449285cf77fe24aa099b5201ad39
Todd Bauer <twbauer8220@eagle.fgcu.edu>
2019-09-24 01:55:13 -0400

    Updated with Javadoc comments, auto-formatted with the Google Java Style Guide, and ran CheckStyle to fix all remaining formatting errors.

--- src/sample/DBManager.java
+++ src/sample/DBManager.java
@@ -1,125 +1,188 @@
+/*
+9/24/2019
+Created by: Todd Bauer
+
+This file holds a class that allows the user to manage a database.
+ */
+
 package sample;
 
 import java.sql.*;
 
+/**
+ * This class manages all database activity including connecting to the
+ * database and executing SQL statements.
+ */
 public class DBManager {
 
-    private Connection conn = null;
-    private PreparedStatement ps = null;
-    private ResultSet rs = null;
-    private boolean isConnectedToDB;
-    boolean getConnectionStatus() {return isConnectedToDB;}
+  private Connection conn = null;
+  private PreparedStatement ps = null;
+  private ResultSet rs = null;
 
-    private String user;
-    private String pword;
+  /**
+   * This attribute tracks if the program is connected to a database.
+   */
+  private boolean isConnectedToDB;
 
-    protected DBManager(String user, String pword) {
-        this.user = user;
-        this.pword = pword;
-        connectToDB();
+  boolean getConnectionStatus() {
+    return isConnectedToDB;
+  }
+
+  /**
+   * Holds the user name for the database connection.
+   */
+  private String user;
+
+  /**
+   * Holds the password for the database connection.
+   */
+  private String pword;
+
+  /**
+   * Default constructor. This gets a username and password from the user, then stores
+   * those values to the class. Finally, it attempts to connect to the database.
+   * @param user  Holds the username for the database
+   * @param pword Holds the password for the database
+   */
+  protected DBManager(String user, String pword) {
+    this.user = user;
+    this.pword = pword;
+    connectToDB();
+  }
+
+  /**
+   * Attempts to connect to the database.
+   */
+  private void connectToDB() {
+
+    // NOTE: USERNAME/PASSWORD IS sa/sa ! This was the default username, and since security
+    // is not a concern with this project, I duplicated it as the password for convenience.
+    try {
+      // Establish a connection to the database and flag the DBManager as connected.
+      Class.forName("org.h2.Driver");
+      conn = DriverManager.getConnection("jdbc:h2:./test", user, pword);
+      isConnectedToDB = true;
+
+      // Cover cases where a connection could not be made.
+    } catch (ClassNotFoundException | SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Create a new table with the specified name. For now, this makes a
+   * predetermined table with 3 columns: UID, first name, and last name.
+   */
+  void createTable(String tableName) {
+    System.out.println("Creating table...");
+
+    try {
+      ps = conn.prepareStatement("CREATE TABLE " + tableName + " (uid INT PRIMARY KEY, " +
+          "fname VARCHAR(255), lname VARCHAR(255))");
+      ps.executeUpdate();
+    } catch (SQLException e) {
+      e.printStackTrace();
+      System.out.print("Could not execute query.");
     }
 
-    private void connectToDB() {
+    System.out.println("Created table.");
+  }
 
-        // NOTE: USERNAME/PASSWORD IS sa/sa ! This was the default username, and since security
-        // is not a concern with this project, I duplicated it as the password for convenience.
-        try {
-            // Establish a connection to the database and flag the DBManager as connected.
-            Class.forName("org.h2.Driver");
-            conn = DriverManager.getConnection("jdbc:h2:./test", user, pword);
-            isConnectedToDB = true;
+  /**
+   * List all of the rows in a given table.
+   * @param tableName Holds the name of the table to be accessed.
+   */
+  void listRowsInTable(String tableName) {
+    try {
+      // Get all rows from the specified table.
+      System.out.println("Executing query...");
+      ps = conn.prepareStatement("SELECT * FROM " + tableName);
+      rs = ps.executeQuery();
+      System.out.println("Executed query.");
 
-        // Cover cases where a connection could not be made.
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
+      // Print the results of the query.
+      while (rs.next()) {
+        System.out.println("UID: " + rs.getInt("uid") + ", First Name: " +
+            rs.getString("fname") + ", Last Name: " +
+            rs.getString("lname"));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+      System.out.print("Could not execute query.");
+    }
+  }
+
+  /**
+   * Insert new rows into the TEST table.
+   * @param uid The universal ID number of the row entry.
+   * @param firstName The first name of the person represented in the row entry.
+   * @param lastName  The last name of the person represented in the row entry.
+   */
+  void insertRowIntoTestTable(int uid, String firstName, String lastName) {
+
+    System.out.println("Inserting records into table...");
+    try {
+      ps = conn.prepareStatement("INSERT INTO TEST VALUES (" + uid + ", '" +
+          firstName + "', " + "'" + lastName + "')");
+      ps.executeUpdate();
+      System.out.println("Inserted records into table.");
+    } catch (SQLException e) {
+      e.printStackTrace();
+      System.out.println("Could not create record.");
+    }
+  }
+
+  /**
+   * Retrieves and prints the columns names (and their data types) in the TEST table.
+   * @param tableName Holds the name of the table to be accessed.
+   */
+  void listColumnNamesInTable(String tableName) {
+    try {
+      System.out.println("Listing column names and their data types...");
+      ps = conn.prepareStatement("SELECT COLUMN_NAME, TYPE_NAME FROM " +
+          "INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + tableName + "'");
+      rs = ps.executeQuery();
+
+      while (rs.next()) {
+        System.out.println("Name: " + rs.getString(1) + ", Data type: " + rs.getString(2));
+      }
+      System.out.println("Listed column names and their data types.");
+    } catch (SQLException e) {
+      e.printStackTrace();
+      System.out.println("Could not retrieve column information.");
+    }
+  }
+
+  /**
+   * Close all objects to tidy up.
+   */
+  void disconnectFromDB() {
+    try {
+        if (rs != null) {
+            rs.close();
         }
+    } catch (Exception e) {
+      System.out.print("Error closing ResultSet.");
+      e.printStackTrace();
     }
-
-    // List all of the rows in a given table.
-    void listRowsInTable(String tableName) {
-        try {
-            // Get all rows from the specified table.
-            System.out.println("Executing query...");
-            ps = conn.prepareStatement("SELECT * FROM " + tableName);
-            rs = ps.executeQuery();
-            System.out.println("Executed query.");
-
-            // Print the results of the query.
-            while (rs.next()) {
-                System.out.println("UID: " + rs.getInt("uid") + ", First Name: " +
-                        rs.getString("fname") + ", Last Name: " +
-                        rs.getString("lname"));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.print("Could not execute query.");
+    try {
+        if (ps != null) {
+            ps.close();
         }
+    } catch (Exception e) {
+      System.out.print("Error closing PreparedStatement.");
+      e.printStackTrace();
     }
-
-    // Close all objects to tidy up.
-    void disconnectFromDB() {
-            try { if (rs != null) rs.close(); } catch (Exception e) {
-                System.out.print("Error closing ResultSet.");
-                e.printStackTrace();
-            } try { if (ps != null) ps.close(); } catch (Exception e) {
-                System.out.print("Error closing PreparedStatement.");
-                e.printStackTrace();
-            } try { if (conn != null) conn.close(); } catch (Exception e) {
-                System.out.print("Error closing Connection.");
-                e.printStackTrace();
-            }
-
-            isConnectedToDB = false;
-            System.out.print("Connection successfully closed.");
-    }
-
-    // Create a new table with the specified name. For now, this makes a
-    // predetermined table with 3 columns: UID, first name, and last name.
-    void createTable(String tableName) {
-        System.out.println("Creating table...");
-
-        try {
-            ps = conn.prepareStatement("CREATE TABLE " + tableName + " (uid INT PRIMARY KEY, " +
-                                       "fname VARCHAR(255), lname VARCHAR(255))");
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.print("Could not execute query.");
+    try {
+        if (conn != null) {
+            conn.close();
         }
-
-        System.out.println("Created table.");
+    } catch (Exception e) {
+      System.out.print("Error closing Connection.");
+      e.printStackTrace();
     }
 
-    // Insert new rows into the TEST table.
-    void insertRowIntoTestTable(int uid, String firstName, String lastName) {
-
-        System.out.println("Inserting records into table...");
-        try {
-            ps = conn.prepareStatement("INSERT INTO TEST VALUES (" + uid + ", '" +
-                                        firstName + "', " + "'" + lastName + "')");
-            ps.executeUpdate();
-            System.out.println("Inserted records into table.");
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Could not create record.");
-        }
-    }
-
-    // Retrieves and prints the columns names (and their data types) in the TEST table.
-    void listColumnNamesInTable(String tableName) {
-        try {
-            System.out.println("Listing column names and their data types...");
-            ps = conn.prepareStatement("SELECT COLUMN_NAME, TYPE_NAME FROM " +
-                    "INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + tableName + "'");
-            rs = ps.executeQuery();
-
-            while (rs.next()) {
-                System.out.println("Name: " + rs.getString(1) + ", Data type: " + rs.getString(2));
-            }
-            System.out.println("Listed column names and their data types.");
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Could not retrieve column information.");
-        }
-    }
+    isConnectedToDB = false;
+    System.out.print("Connection successfully closed.");
+  }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,3 +1,11 @@
+/*
+9/24/2019
+Created by: Todd Bauer
+
+This program is a simple JavaFX program with 3 tabs and the ability to connect to a
+database and execute SQL statements.
+ */
+
 package sample;
 
 import javafx.application.Application;
@@ -7,106 +15,106 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import java.sql.*;
 
 public class Main extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
 
-    @Override
-    public void start(Stage primaryStage) {
+  public static void main(String[] args) {
+    launch(args);
+  }
 
-        primaryStage.setTitle("JavaFX Welcome");
+  @Override
+  public void start(Stage primaryStage) {
 
-        // Create tabs in the application and do not let the user close them.
-        TabPane tabPane = new TabPane();
-        tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+    primaryStage.setTitle("JavaFX Welcome");
 
-        // Login pane creation. This will be inserted into the TabPane.
-        GridPane loginPane = new GridPane();
-        loginPane.setAlignment(Pos.CENTER);
-        loginPane.setHgap(10);
-        loginPane.setVgap(10);
-        loginPane.setPadding(new Insets(25, 25, 25, 25));
+    // Create tabs in the application and do not let the user close them.
+    TabPane tabPane = new TabPane();
+    tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
 
-        Text scenetitle = new Text("Welcome");
-        scenetitle.setId("welcome-text");
-        loginPane.add(scenetitle, 0, 0, 2, 1);
+    // Login pane creation. This will be inserted into the TabPane.
+    GridPane loginPane = new GridPane();
+    loginPane.setAlignment(Pos.CENTER);
+    loginPane.setHgap(10);
+    loginPane.setVgap(10);
+    loginPane.setPadding(new Insets(25, 25, 25, 25));
 
-        Label userName = new Label("User Name:");
-        loginPane.add(userName, 0, 1);
+    Text scenetitle = new Text("Welcome");
+    scenetitle.setId("welcome-text");
+    loginPane.add(scenetitle, 0, 0, 2, 1);
 
-        TextField userTextField = new TextField();
-        loginPane.add(userTextField, 1, 1);
+    Label userName = new Label("User Name:");
+    loginPane.add(userName, 0, 1);
 
-        Label pw = new Label("Password:");
-        loginPane.add(pw, 0, 2);
+    TextField userTextField = new TextField();
+    loginPane.add(userTextField, 1, 1);
 
-        PasswordField pwBox = new PasswordField();
-        loginPane.add(pwBox, 1, 2);
+    Label pw = new Label("Password:");
+    loginPane.add(pw, 0, 2);
 
-        // Create the login button.
-        Button btn = new Button("Sign in");
-        HBox hbBtn = new HBox(10);
-        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-        hbBtn.getChildren().add(btn);
-        loginPane.add(hbBtn, 1, 4);
+    PasswordField pwBox = new PasswordField();
+    loginPane.add(pwBox, 1, 2);
 
-        // Create a text label for printing errors and confirmations.
-        final Text actiontarget = new Text();
-        actiontarget.setId("actiontarget");
-        loginPane.add(actiontarget, 1, 5);
+    // Create the login button.
+    Button btn = new Button("Sign in");
+    HBox hbBtn = new HBox(10);
+    hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+    hbBtn.getChildren().add(btn);
+    loginPane.add(hbBtn, 1, 4);
 
-        // Create a new tab, insert the login pane, then add it to the TabPane.
-        Tab loginTab = new Tab();
-        loginTab.setText("Login");
-        loginTab.setContent(loginPane);
-        loginTab.setClosable(false);
-        tabPane.getTabs().add(loginTab);
+    // Create a text label for printing errors and confirmations.
+    final Text actiontarget = new Text();
+    actiontarget.setId("actiontarget");
+    loginPane.add(actiontarget, 1, 5);
 
-        // Create the second tab. (This is blank for now)
-        Tab tab2 = new Tab();
-        tab2.setText("Tab 2");
-        tabPane.getTabs().add(tab2);
+    // Create a new tab, insert the login pane, then add it to the TabPane.
+    Tab loginTab = new Tab();
+    loginTab.setText("Login");
+    loginTab.setContent(loginPane);
+    loginTab.setClosable(false);
+    tabPane.getTabs().add(loginTab);
 
-        // Create the third tab. (This is blank for now)
-        Tab tab3 = new Tab();
-        tab3.setText("Tab 3");
-        tabPane.getTabs().add(tab3);
+    // Create the second tab. (This is blank for now)
+    Tab tab2 = new Tab();
+    tab2.setText("Tab 2");
+    tabPane.getTabs().add(tab2);
 
-        // When the user clicks the login button, connect to the database
-        // and attempt to execute SQL statements.
-        btn.setOnAction(event -> {
+    // Create the third tab. (This is blank for now)
+    Tab tab3 = new Tab();
+    tab3.setText("Tab 3");
+    tabPane.getTabs().add(tab3);
 
-            // Create a new database manager object for database manipulation.
-            DBManager db = new DBManager(userTextField.getText(), pwBox.getText());
+    // When the user clicks the login button, connect to the database
+    // and attempt to execute SQL statements.
+    btn.setOnAction(event -> {
 
-            // If a connection was established, run some SQL queries to test function.
-            // Else, inform the user that a connection could not be made.
-            if (db.getConnectionStatus()) {
-                //db.createTable("TEST");
-                //db.insertRowIntoTestTable(3, "Bill", "Burr");
-                db.listRowsInTable("TEST");
-                db.listColumnNamesInTable("TEST");
-                db.disconnectFromDB();
-                actiontarget.setText("Connection successful.");
-            } else {
-                actiontarget.setText("Connection failed.\nCheck name and password.");
-            }
-        });
+      // Create a new database manager object for database manipulation.
+      DBManager db = new DBManager(userTextField.getText(), pwBox.getText());
 
-        // Create and show the scene from the TabPane.
-        Scene scene = new Scene(tabPane, 300, 275);
-        primaryStage.setScene(scene);
-        scene.getStylesheets().add(Main.class.getResource("Main.css").toExternalForm());
-        primaryStage.show();
-    }
+      // If a connection was established, run some SQL queries to test function.
+      // Else, inform the user that a connection could not be made.
+      if (db.getConnectionStatus()) {
+        //db.createTable("TEST");
+        //db.insertRowIntoTestTable(3, "Bill", "Burr");
+        db.listRowsInTable("TEST");
+        db.listColumnNamesInTable("TEST");
+        db.disconnectFromDB();
+        actiontarget.setText("Connection successful.");
+      } else {
+        actiontarget.setText("Connection failed.\nCheck name and password.");
+      }
+    });
+
+    // Create and show the scene from the TabPane.
+    Scene scene = new Scene(tabPane, 300, 275);
+    primaryStage.setScene(scene);
+    scene.getStylesheets().add(Main.class.getResource("Main.css").toExternalForm());
+    primaryStage.show();
+  }
 }
